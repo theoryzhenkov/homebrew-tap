dabd3e4949677478a39524c2bff7d388ad871543
Theo Ryzhenkov <account+github@the-o.space>
2025-12-26 06:13:55 +0400

    chore(formula:slap): v0.2.1 slap release

--- Formula/slap.rb
+++ Formula/slap.rb
@@ -1,17 +1,17 @@
 class Slap < Formula
   desc "Create files and directories with ease - touch, but slappier"
   homepage "https://github.com/theoryzhenkov/slap"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/theoryzhenkov/slap/releases/download/v#{version}/slap-arm64.tar.gz"
-      sha256 "df3f8f3656a707f95e98aeed369a9c497f7a9a26b33a0f8292c8659439016171"
+      sha256 "b072f0a998da68d278950a7aa3a86faff884190e2e5f6ac35e8a0a446364da6b"
     end
     on_intel do
       url "https://github.com/theoryzhenkov/slap/releases/download/v#{version}/slap-x86_64.tar.gz"
-      sha256 "cef95ab9eeca65cb2499058eb10fe1425df416a9dbffbcf6017f1875da51bd5d"
+      sha256 "0bd9c084eb6f4be94477c0348cb118c880278034ea892820746bbd836d76d426"
     end
   end
 
